48352e9324da089e68cc980fb163e9cacda38235
Jeren <jerenshamuhamedova@gmail.com>
2021-03-17 18:01:38 -0500

    jeren update

--- src/test/java/com/library/dataBaseTesting/Connection.java
+++ src/test/java/com/library/dataBaseTesting/Connection.java
@@ -1,22 +1,22 @@
 package com.library.dataBaseTesting;
 
 import com.library.utilities.ConfigurationReader;
+import com.library.utilities.DB_Utility;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Connection {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
-        String url = ConfigurationReader.getProperty("library1.database.url");
-        String username = ConfigurationReader.getProperty("library1.database.username");
-        String password = ConfigurationReader.getProperty("library1.database.password");
+//        String url = ConfigurationReader.getProperty("library1.database.url");
+//        String username = ConfigurationReader.getProperty("library1.database.username");
+//        String password = ConfigurationReader.getProperty("library1.database.password");
+//
+//        Connection connection = DriverManager.getConnection(url, username, password);
+//        Statement statement = connection.
 
-        try {
-            Connection connection = DriverManager.getConnection(url, username, password);
-        } catch (SQLException e) {
-            e.getMessage();
-        }
+        DB_Utility.createConnection();
+        DB_Utility.runQuery("SELECT * FROM books");
 
     }
 }
